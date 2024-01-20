8a84252b8286470f0eea3aaeac901501fefb460b
metaory <metaory@gmail.com>
2024-01-20 14:54:55 +0800

    updated wezterm tpl

--- templates/wezterm-color.lua
+++ templates/wezterm-color.lua
@@ -1,15 +1,33 @@
-return {
-  foreground = "$XFG",
-  background = "$XBG",
-  ansi= {
-    "$C00",
-    "$C01",
-    "$C02",
-    "$C03",
-    "$C04",
-    "$C05",
-    "$C06",
-    "$C07",
-    "$C08"
+-- mxc: path=$XDG_CONFIG_HOME/wezterm/color.lua label=wezterm
+
+local M = {}
+
+M.apply = function(cfg)
+  cfg.color_scheme = "tokyonight_night"
+  cfg.colors = {
+    foreground = "$XFG",
+    background = "$XBG",
+    ansi = {
+      "$C00",
+      "$C01",
+      "$C02",
+      "$C03",
+      "$C04",
+      "$C05",
+      "$C06",
+      "$C07",
+    },
+    brights = {
+      "$C08",
+      "$C09",
+      "$C10",
+      "$C11",
+      "$C12",
+      "$C13",
+      "$C14",
+      "$C15",
+    },
   }
-}
+end
+
+return M
