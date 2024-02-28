691df30abb2621cb06b5842ab5b9e98db423a0f9
metaory <metaory@gmail.com>
2024-02-28 17:30:33 +0800

    sample config update

--- templates/base64.lua
+++
@@ -1,9 +0,0 @@
--- mxc: path=/tmp/mxc/luaXxxxxaa label=zelda
-
-local base16 = {
-  base00="${XBG}"; base01="${SK3}"; base02="${WK4}"; base03="${EK5}";
-  base04="${EK6}"; base05="${WK7}"; base06="${SK8}"; base07="${WK9}";
-  base08="${C01}"; base09="${C03}"; base0A="${C03}"; base0B="${C05}";
-  base0C="${C02}"; base0D="${C04}"; base0E="${WBG}"; base0F="${SBG}";
-}
-return base16
